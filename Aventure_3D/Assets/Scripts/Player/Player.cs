82c87429886218874cb8ada41b0c0e49b119c57e
xSuui <samuel2012rogerio@gmail.com>
2023-11-10 14:58:49 -0300

    add run to player

--- Aventure_3D/Assets/Scripts/Player/Player.cs
+++ Aventure_3D/Assets/Scripts/Player/Player.cs
@@ -16,6 +16,11 @@ public class Player : MonoBehaviour
 
     public KeyCode jumpKeycode = KeyCode.Space;
 
+    [Header("Run Setup")]
+    public KeyCode keyRun = KeyCode.LeftShift;
+    public float speedRun = 1.5f;
+
+
     void Update()
     {
         transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
@@ -37,9 +42,22 @@ public class Player : MonoBehaviour
         vSpeed -= gravity * Time.deltaTime;
         speedVector.y = vSpeed;
 
+        var isWalking = inputAxisVertical != 0;
+        if (isWalking)
+        {
+            if (Input.GetKey(keyRun))
+            {
+                speedVector *= speedRun;
+                animator.speed = speedRun;
+            }
+            else
+            {
+                animator.speed = 1;
+            }
+        }
+
         characterController.Move(speedVector * Time.deltaTime);
-           
-        
+         
         animator.SetBool("Run", inputAxisVertical != 0);
 
         /*if (inputAxisVertical != 0)
