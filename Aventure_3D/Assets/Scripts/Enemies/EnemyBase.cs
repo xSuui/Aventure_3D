bead0cd9815830c12fff5f8a9a60b1d4aafcceab
xSuui <samuel2012rogerio@gmail.com>
2023-11-27 17:50:46 -0300

    Making enemy affected by weapon

--- Aventure_3D/Assets/Scripts/Enemies/EnemyBase.cs
+++ Aventure_3D/Assets/Scripts/Enemies/EnemyBase.cs
@@ -6,9 +6,9 @@ using Animation;
 
 namespace Enemy
 { 
-    public class EnemyBase : MonoBehaviour
+    public class EnemyBase : MonoBehaviour, IDamageable
     {
-
+        public Collider collider;
         public float startLife = 10f;
 
         [SerializeField] private float _currentLife;
@@ -46,6 +46,7 @@ namespace Enemy
 
         protected virtual void OnKill() 
         {
+            if (collider != null) collider.enabled = false;
             Destroy(gameObject, 3f);
             PlayAnimationByTrigger(AnimationType.DEATH);
         }
@@ -80,6 +81,12 @@ namespace Enemy
             {
                 OnDamage(5f);
             }
+        }
+
+        public void Damage(float damage)
+        {
+            Debug.Log("Damage");
+            OnDamage(damage);
         }
 
     }
