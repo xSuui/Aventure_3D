97b001ba9be646d55f4a4b30082697fd68388987
xSuui <samuel2012rogerio@gmail.com>
2023-12-01 13:05:01 -0300

    add chest to game

---
+++ Aventure_3D/Assets/Scripts/Chest/ChestBase.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ChestBase : MonoBehaviour
+{
+    public Animator animator;
+    public string triggerOpen = "Open";
+
+    [Header("Notification")]
+    public GameObject notification;
+    public float tweenDuration = .2f;
+    public Ease tweenEase = Ease.OutBack;
+    private float startScale;
+
+    void Start()
+    {
+        startScale = notification.transform.localScale.x;
+        HideNotification();
+    }
+
+    [NaughtyAttributes.Button]
+    private void OpenChest()
+    {
+        animator.SetTrigger(triggerOpen);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player p = other.transform.GetComponent<Player>();
+        if(p != null)
+        {
+            ShowNotification();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Player p = other.transform.GetComponent<Player>();
+        if (p != null)
+        {
+            HideNotification();
+        }
+    }
+
+    [NaughtyAttributes.Button]
+    private void ShowNotification()
+    {
+        notification.SetActive(true);
+        notification.transform.localScale = Vector3.zero;
+        notification.transform.DOScale(startScale, tweenDuration);
+    }
+
+    [NaughtyAttributes.Button]
+    private void HideNotification()
+    {
+        notification.SetActive(false);
+    }
+}
