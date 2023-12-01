644f03857924388cad033a436cbbae74df2c0d36
xSuui <samuel2012rogerio@gmail.com>
2023-12-01 15:06:01 -0300

    add destructable objects

---
+++ Aventure_3D/Assets/Scripts/Items/DestructableItems/DestructableItemBase.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DestructableItemBase : MonoBehaviour
+{
+    public HealthBase healthBase;
+
+    public float shakeDuration = .1f;
+    public int shakeForce = 1;
+
+    private void OnValidate()
+    {
+        if (healthBase == null) healthBase = GetComponent<HealthBase>();
+    }
+
+    private void Awake()
+    {
+        OnValidate();
+        healthBase.OnDamage += OnDamage;
+    }
+
+    private void OnDamage(HealthBase h)
+    {
+        transform.DOShakeScale(shakeDuration, Vector3.up/2, shakeForce);
+    }
+}
