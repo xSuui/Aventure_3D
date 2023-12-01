6c794698406cfca41a84dc768bfec05a26592bcb
xSuui <samuel2012rogerio@gmail.com>
2023-11-30 21:04:08 -0300

    add items to inventory

--- Aventure_3D/Assets/Scripts/Items/IitemManager.cs
+++ Aventure_3D/Assets/Scripts/Items/IitemManager.cs
@@ -69,6 +69,7 @@ namespace Items
     {
         public ItemType itemType;
         public SOInt soInt;
+        public Sprite icon;
     }
 
 }
---
+++ Aventure_3D/Assets/Scripts/Items/ItemLayout.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Items
+{
+
+    public class ItemLayout : MonoBehaviour
+    {
+        public ItemSetup _currSetup;
+
+        public Image uiIcon;
+        public TextMeshProUGUI uiValue;
+
+        public void Load(ItemSetup setup)
+        {
+            _currSetup = setup;
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            uiIcon.sprite = _currSetup.icon;
+        }
+
+        private void Update()
+        {
+            uiValue.text = _currSetup.soInt.value.ToString();
+        }
+    }
+
+}
