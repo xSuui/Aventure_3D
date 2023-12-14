7759c9c47b02ba0868cea7e5741bfa5f3ec07eae
xSuui <samuel2012rogerio@gmail.com>
2023-12-14 11:34:45 -0300

    add sound manager

--- Aventure_3D/Assets/Scripts/Audio/SoundManager.cs
+++ Aventure_3D/Assets/Scripts/Audio/SoundManager.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ebac.Core.Singleton;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager : Singleton<SoundManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<MusicSetup> musicSetups;
+    public List<SFXSetup> sfxSetups;
+
+    public AudioSource musicSource;
+
+
+    public void PlayMusicByType(MusicType musicType)
     {
-        
+        var music = GetMusicByType(musicType);
+        musicSource.clip = music.audioClip;
+        musicSource.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    public MusicSetup GetMusicByType(MusicType musicType)
     {
-        
+        return musicSetups.Find(i => i.musicType == musicType);
+    }
+
+    public SFXSetup GetSFXByType(SFXType sfxType)
+    {
+        return sfxSetups.Find(i => i.sFXTyp == sfxType);
     }
 }
+
+
+   public enum MusicType
+    {
+        TYPE_01,
+        TYPE_02,
+        TYPE_03
+    }   
+
+    [System.Serializable]
+    public class MusicSetup
+    {
+        public MusicType musicType;
+        public AudioClip audioClip;
+    }
+
+
+
+    public enum SFXType
+    {
+        TYPE_01,
+        TYPE_02,
+        TYPE_03
+    }
+
+    [System.Serializable]
+    public class SFXSetup
+    {
+        public SFXType sFXTyp;
+        public AudioClip audioClip;
+    }
